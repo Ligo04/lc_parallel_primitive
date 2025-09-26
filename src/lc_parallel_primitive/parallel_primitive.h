f81fee20fcbdca8b968ac9129c635468b1c1b19a
ligo <1569731402@qq.com>
2025-09-26 13:11:30 +0800

    🐞 fix: multi template struct

--- src/lc_parallel_primitive/parallel_primitive.h
+++ src/lc_parallel_primitive/parallel_primitive.h
@@ -5,5 +5,19 @@
  * @Last Modified time: 2025-09-23 00:04:35
  */
 #pragma once
+//#include "template_struct.h"
 #include <lc_parallel_primitive/warp/warp_scan.h>
 #include <lc_parallel_primitive/device/device_reduce.h>
+
+
+template <typename valueType, size_t Rows, size_t Cols>
+struct MyMatrix
+{
+    std::array<valueType, Rows * Cols> data{};  // 用 array 存储数据
+};
+
+#define LUISA_MATERIX_TEMPLATE()                                               \
+    template <typename valueType, size_t Rows, size_t Cols>
+#define LUISA_MATERIX() MyMatrix<valueType, Rows, Cols>
+
+LUISA_TEMPLATE_STRUCT(LUISA_MATERIX_TEMPLATE, LUISA_MATERIX, data){};
